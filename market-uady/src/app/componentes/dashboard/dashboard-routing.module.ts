af7ff872937a84134f233257c0b6f3840843b66b
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2023-09-30 19:16:20 -0600

    correccion dashboard routing

--- market-uady/src/app/componentes/dashboard/dashboard-routing.module.ts
+++ market-uady/src/app/componentes/dashboard/dashboard-routing.module.ts
@@ -4,14 +4,10 @@ import { InicioComponent } from './inicio/inicio.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  {
-    path: '', component: DashboardComponent, children: [
-      { path: 'inicio', component: InicioComponent },
-      { path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(x => x.UsuarioModule) },
-      { path: 'publicacion', loadChildren: () => import('./publicacion/publicacion.module').then(x => x.PublicacionModule) },
-
-    ]
-  }
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
+  { path: 'usuario', loadChildren: () => import('./usuario/usuario.module').then(x => x.UsuarioModule) },
+  { path: 'publicacion', loadChildren: () => import('./publicacion/publicacion.module').then(x => x.PublicacionModule) },
 ];
 
 @NgModule({
