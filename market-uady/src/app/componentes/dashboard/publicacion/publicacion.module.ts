120af5631292ae13afa25b2b20cbd6cade330795
Axel Anaya <axel.ana25@gmail.com>
2023-10-10 22:03:02 -0500

    Imported material components

--- market-uady/src/app/componentes/dashboard/publicacion/publicacion.module.ts
+++ market-uady/src/app/componentes/dashboard/publicacion/publicacion.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { PublicacionRoutingModule } from './publicacion-routing.module';
 import { FormularioPublicacionComponent } from './formulario-publicacion/formulario-publicacion.component';
 import { VistaPublicacionComponent } from './vista-publicacion/vista-publicacion.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [
-    FormularioPublicacionComponent,
-    VistaPublicacionComponent
-  ],
+  declarations: [FormularioPublicacionComponent, VistaPublicacionComponent],
   imports: [
     CommonModule,
-    PublicacionRoutingModule
-  ]
+    PublicacionRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSelectModule,
+  ],
 })
-export class PublicacionModule { }
+export class PublicacionModule {}
