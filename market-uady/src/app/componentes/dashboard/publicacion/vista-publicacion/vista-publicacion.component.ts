202104ce5e5e4f6f710a3b258e56f6c091b94188
Axel Anaya <axel.ana25@gmail.com>
2023-10-10 22:03:32 -0500

    Updated code format

--- market-uady/src/app/componentes/dashboard/publicacion/vista-publicacion/vista-publicacion.component.ts
+++ market-uady/src/app/componentes/dashboard/publicacion/vista-publicacion/vista-publicacion.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-vista-publicacion',
   templateUrl: './vista-publicacion.component.html',
-  styleUrls: ['./vista-publicacion.component.css']
+  styleUrls: ['./vista-publicacion.component.css'],
 })
-export class VistaPublicacionComponent {
-
-}
+export class VistaPublicacionComponent {}
