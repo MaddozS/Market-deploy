d28ab1b06fa6115230e40709c1891a77485b24c3
asha-gc <56806594+asha-gc@users.noreply.github.com>
2023-10-27 17:38:59 -0600

    Add files via upload

--- market-uady/src/app/componentes/dashboard/usuario/formulario-usuario/formulario-usuario.component.ts
+++ market-uady/src/app/componentes/dashboard/usuario/formulario-usuario/formulario-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { GeneralService } from 'src/app/servicios/general.service';
 
 @Component({
@@ -8,5 +8,20 @@ import { GeneralService } from 'src/app/servicios/general.service';
   styleUrls: ['./formulario-usuario.component.css']
 })
 export class FormularioUsuarioComponent {
+  formulario: FormGroup;
   
+  constructor(private fb: FormBuilder) {
+    this.formulario = fb.group({
+      nombre: [{ value: 'Daniel Eduardo Gutierrez Delfin', disabled: true }, Validators.required],
+      facultad: ['Facultad de Matemáticas', Validators.required], // Agrega otras facultades aquí
+      correo: [{ value: 'a16383940@alumnos.uady.mx', disabled: true }, Validators.required],
+      numeroContacto: [''],
+      imagenPerfil: ['']
+    });
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    // Realiza las acciones que desees con el archivo seleccionado
+  }
 }
