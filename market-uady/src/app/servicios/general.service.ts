4519cb3da883e004e0b2c2e6a77bfacae623a8c7
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2023-10-29 14:19:41 -0600

    guarda

--- market-uady/src/app/servicios/general.service.ts
+++ market-uady/src/app/servicios/general.service.ts
@@ -1,38 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeneralService {
-
-  private URL = "http://localhost:8080";
+  private URL = 'http://localhost:8080';
   private headers: HttpHeaders;
+  private loggedIn = false;
 
   constructor(private http: HttpClient) {
     this.headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+      Authorization: `Bearer ${sessionStorage.getItem('token')}`,
     });
-
   }
 
   private getHeaders(): HttpHeaders {
+    const tokenString = `Bearer ${sessionStorage.getItem('token')}`;
+    const tokenWithoutQuotes = tokenString.replace(/^Bearer\s+"(.*?)"$/, 'Bearer $1');
 
-    let tokenString = `Bearer ${sessionStorage.getItem('token')}`;
-    let tokenWithoutQuotes = tokenString.replace(/^Bearer\s+"(.*?)"$/, 'Bearer $1');
-    
     return new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': tokenWithoutQuotes
+      Authorization: tokenWithoutQuotes,
     });
   }
 
   login(data: any): Observable<any> {
-    return this.http.post('http://localhost:8000/api/login', data);
-  }
+    return this.http.post('http://localhost:8000/api/login', data).pipe(
+      tap(() => {
+        // Actualiza el estado de autenticación cuando el usuario inicia sesión exitosamente
+        this.loggedIn=true;
+        sessionStorage.setItem('isLoggedIn', 'true');
 
+      })
+    );
+  }
+  isLoggedIn(): boolean {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+  this.loggedIn = isLoggedIn === 'true';
+  console.log(this.loggedIn);
+  return this.loggedIn;
+  }
   guardarUsuario(usuario: any): Observable<any> {
     return this.http.post('http://localhost:8000/api/register', usuario);
   }
@@ -40,22 +50,15 @@ export class GeneralService {
   obtenerDatosFiltro(): Observable<any> {
     return this.http.get('http://localhost:8000/api/filters', { headers: this.getHeaders() });
   }
-   obtenerPublicacionesFiltrado(body:any): Observable<any> {
-
-    return this.http.post('http://localhost:8000/api/publications/search',  body, { headers: this.getHeaders() });
+  obtenerPublicacionesFiltrado(body: any): Observable<any> {
+    return this.http.post('http://localhost:8000/api/publications/search', body, { headers: this.getHeaders() });
   }
 
-  obtenerPublicacionesInicio(): Observable<any>{
+  obtenerPublicacionesInicio(): Observable<any> {
     return this.http.get('http://localhost:8000/api/publications', { headers: this.getHeaders() });
   }
 
-  obtenerFacultades():Observable<any>{
+  obtenerFacultades(): Observable<any> {
     return this.http.get('http://localhost:8000/api/facultades', { headers: this.getHeaders() });
   }
-
-
-  
-	
-
-
 }
