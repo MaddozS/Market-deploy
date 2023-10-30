5e4d2a3c4f3db8109c76376c2d7ae7f513a58254
Axel Anaya <axel.ana25@gmail.com>
2023-10-29 22:35:57 -0600

    Added new types

--- market-uady/src/app/types.ts
+++ market-uady/src/app/types.ts
@@ -1,7 +1,21 @@
 export type Publicacion = {
-  imagenes: File[];
+  imagenes: File[] | string[];
   titulo: string;
   descripcion: string;
   precio: number;
   categoria: 'producto' | 'servicio';
+};
+// omit images and precio
+export type PublicacionEdit = Omit<Publicacion, 'imagenes' | 'precio'> & {
+  imagenes: string[] | null;
+  precio: string | number;
+};
+
+type PublicationInnerResponse = Omit<Publicacion, 'imagenes'> & {
+  imagenes: string[];
+};
+
+export type PublicacionResponse = {
+  publicacion: PublicationInnerResponse;
+  vendedor: any;
 };
