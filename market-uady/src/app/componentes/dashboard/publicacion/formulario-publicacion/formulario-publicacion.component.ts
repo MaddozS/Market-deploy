b9294fbd764a3e179cdc6a82c6697bbf9048d0c7
Axel Anaya <axel.ana25@gmail.com>
2023-10-29 22:36:47 -0600

    Added logic to update publication

--- market-uady/src/app/componentes/dashboard/publicacion/formulario-publicacion/formulario-publicacion.component.ts
+++ market-uady/src/app/componentes/dashboard/publicacion/formulario-publicacion/formulario-publicacion.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { GeneralService } from 'src/app/servicios/general.service';
-import { Publicacion } from 'src/app/types';
+import { Publicacion, PublicacionEdit } from 'src/app/types';
+import { ActivatedRoute } from '@angular/router';
+import { MAX_FILE_SIZE, MAX_IMAGES_PER_PUBLICATION } from 'src/app/constants/const';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-formulario-publicacion',
@@ -12,7 +15,12 @@ export class FormularioPublicacionComponent {
   form!: FormGroup;
   uploadedImages: string[] = [];
   files: File[] = [];
-  publicacion: Publicacion = {
+  imagesCount = 0; // max limit of images is 5
+  isEditing = false;
+  currentId: number | null = null;
+  imagesCountShouldReset = false;
+
+  publicacion: PublicacionEdit = {
     imagenes: [],
     titulo: '',
     descripcion: '',
@@ -20,36 +28,123 @@ export class FormularioPublicacionComponent {
     categoria: 'producto',
   };
 
-  constructor(private servicio: GeneralService) {}
+  constructor(
+    private servicio: GeneralService,
+    private route: ActivatedRoute,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  mostrarMensaje(mensaje: string) {
+    this._snackBar.open(mensaje, 'Cerrar', {
+      duration: 5000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  getPublicacion(id: number) {
+    this.servicio.obtenerPublicacion(id).subscribe({
+      next: (response) => {
+        const { publicacion } = response;
+        this.publicacion = { ...this.publicacion, ...publicacion, imagenes: null };
+        // Get images url from the publicacion and transform them into an File array
+        // to be able to show them in the form
+        // this.files = files;
+        this.uploadedImages = publicacion.imagenes;
+        this.isEditing = true;
+        this.imagesCountShouldReset = true;
+        this.currentId = id;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 
   ngOnInit() {
-    this.form = new FormGroup({
-      imagenes: new FormControl(this.publicacion.imagenes, Validators.required),
-      titulo: new FormControl(this.publicacion.titulo, Validators.required),
-      descripcion: new FormControl(this.publicacion.descripcion, Validators.required),
-      precio: new FormControl(this.publicacion.precio, [Validators.required, Validators.min(0)]),
-      categoria: new FormControl(this.publicacion.categoria, Validators.required),
+    // Getting the id from the URL
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      // Initialize the form depending if the user is editing or creating a publication
+      this.form = new FormGroup({
+        titulo: new FormControl(this.publicacion.titulo, [Validators.required]),
+        descripcion: new FormControl(this.publicacion.descripcion, [Validators.required]),
+        precio: new FormControl(this.publicacion.precio, [Validators.required, Validators.min(0.01)]),
+        categoria: new FormControl(this.publicacion.categoria, [Validators.required]),
+        imagenes: new FormControl(this.publicacion.imagenes),
+      });
+      if (id) {
+        this.getPublicacion(+id);
+      }
+      //images are required only when creating a publication
+      if (!this.isEditing) {
+        this.form.removeControl('imagenes');
+        this.form.addControl('imagenes', new FormControl(this.publicacion.imagenes, [Validators.required]));
+      }
     });
   }
 
   onImageSelected(event: any) {
-    const files = event.target.files;
-    if (files) {
-      for (let i = 0; i < files.length; i++) {
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-          this.uploadedImages.push(e.target.result);
-        };
-        reader.readAsDataURL(files[i]);
-        this.files.push(files[i]);
+    const filesUploaded: File[] = event.target.files;
+    if (!filesUploaded) {
+      return;
+    }
+
+    // If the user is editing a publication, we remove the old images from the array
+    // and add the new ones
+    if (this.isEditing && this.imagesCountShouldReset) {
+      this.uploadedImages = [];
+      this.files = [];
+      this.imagesCount = 0;
+      this.imagesCountShouldReset = false;
+    }
+
+    // Check if the images does not exceed the limit of images
+    if (this.imagesCount + filesUploaded.length > MAX_IMAGES_PER_PUBLICATION) {
+      console.log('El número máximo de imágenes por publicación es 5');
+      return;
+    }
+
+    for (const file of filesUploaded) {
+      if (file.size > MAX_FILE_SIZE) {
+        alert('El tamaño de la imagen no puede ser mayor a 4MB');
+        return;
       }
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.uploadedImages.push(e.target.result);
+      };
+      reader.readAsDataURL(file);
+      this.files.push(file);
+      this.imagesCount++;
     }
   }
 
   guardarPublicacion() {
-    this.servicio.crearPublicacion(this.publicacion, this.files).subscribe({
+    this.servicio.crearPublicacion(this.publicacion as Publicacion, this.files).subscribe({
+      next: (response) => {
+        this.mostrarMensaje('Publicación creada!');
+        this.form.reset();
+        this.uploadedImages = [];
+        this.files = [];
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  guardarCambios() {
+    if (!this.currentId) {
+      return;
+    }
+
+    this.servicio.editarPublicacion(this.currentId, this.publicacion as PublicacionEdit, this.files).subscribe({
       next: (response) => {
         console.log(response);
+        this.mostrarMensaje('Publicación editada!');
       },
       error: (error) => {
         console.log(error);
